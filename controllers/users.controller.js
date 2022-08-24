bda2f31fa776ca98488ddf06b5c5d4c170bd5962
ferdousr3 <ferdoour1992@gmail.com>
2022-08-24 23:41:27 +0600

    #44 add put system

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -14,7 +14,7 @@ const getAllUsers = async (req, res) => {
 const getOneUser = async (req, res) => {
   try {
     const user = await Users.findOne({ email: req.params.email });
-    console.log(user)
+    console.log(user);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
@@ -22,21 +22,38 @@ const getOneUser = async (req, res) => {
 };
 
 const putOneUser = async (req, res) => {
+  // try {
+  //   const user = await Users.findOneAndUpdate(
+  //     { email: req.params.email },
+  //     {
+  //       upsert: true,
+  //       new: true,
+  //     },
+  //     (err, data) => {
+  //       if (err) {
+  //         console.log(error);
+  //       } else {
+  //         console.log(data);
+  //       }
+  //     }
+  //   );
+  //   res.status(200).json(user);
+  // } catch (error) {
+  //   res.status(500).send(error.message);
+  // }
+
   try {
-    const user = await Users.findOneAndUpdate(
-      { email: req.params.email },
-      {
-        upsert: true,
-        new: true,
-      },
-      (err, data) => {
-        if (err) {
-          console.log(error);
-        } else {
-          console.log(data);
-        }
-      }
-    );
+    const email = req.params.email;
+    const user = req.body;
+    const filter = { email: email };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: user,
+    };
+    const result = await Users.updateOne(filter, updateDoc, options);
+    // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+    //   expiresIn: "12h",
+    // });
     res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
